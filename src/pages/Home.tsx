a0681fe7a25908ce4445c7b253bbe3019ad3611f
Mariane <marianehds@outlook.com>
2026-02-13 21:27:33 -0300

    feat: add image carousel to Home page with new Pokémon slides

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,52 @@
+import { Carousel, Col, Input, Row } from "antd";
+import { slides } from "../assets/images/slides";
+
+const contentStyle: React.CSSProperties = {
+  margin: 0,
+  textAlign: "center",
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+  justifyContent: "center",
+  position: "relative",
+  backgroundColor: "#fff",
+};
+
 export function Home() {
-  return <h1>Home</h1>;
+  return (
+    <>
+      <Row
+        justify="center"
+        align="middle"
+        style={{ minHeight: "90vh" }}
+        gutter={{ xs: 8, sm: 16, md: 24, lg: 32 }}
+      >
+        <Col span={6}>
+          <Carousel autoplay dots={false} effect="fade" autoplaySpeed={4000} >
+            {slides.map((slide, index) => (
+              <div key={index}>
+                <div style={contentStyle}>
+                  <img
+                    src={slide.image}
+                    alt={slide.name}
+                    style={{
+                      width: "100%",
+                      objectFit: "cover",
+                      height: "500px",
+                    }}
+                  />
+                  <div>
+                    <h2>{slide.name}</h2>
+                  </div>
+                </div>
+              </div>
+            ))}
+          </Carousel>
+        </Col>
+        <Col span={6}>coluna de pesquisa
+        <Input size="large" />
+        </Col>
+      </Row>
+    </>
+  );
 }
