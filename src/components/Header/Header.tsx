ef3ea2f809ca9c1df839e4dff8fae67988bf0414
Mariane <marianehds@outlook.com>
2026-02-18 17:08:48 -0300

    refactor: remove unused dispatch and clearSearch function from Header component

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,19 +1,14 @@
 import { Button } from "antd";
 import { ArrowLeftOutlined } from "@ant-design/icons";
 import { useLocation, useNavigate } from "react-router-dom";
-import { useAppDispatch } from "../../store/hooks";
-import { clearSearch } from "../../store/pokemon/slice";
 import "./Header.css";
 
 export function Header() {
   const location = useLocation();
   const navigate = useNavigate();
-  const dispatch = useAppDispatch();
 
   const isDetailsPage = location.pathname.startsWith("/pokemon/");
-
   const handleGoBack = () => {
-    dispatch(clearSearch());
     navigate("/");
   };
 
